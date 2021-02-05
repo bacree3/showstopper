c0d51084f1d82068d9fac457351d1280aa8bf391
s-quiroz35 <charitymagazinegreen@gmail.com>
2021-02-05 10:03:31 -0500

    same code but c l e a n

--- search/index.py
+++ search/index.py
@@ -6,6 +6,16 @@ import math
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
+##################### TEMPORARY #######################
+# This accesses imdb's list of the 250 top rated movies
+# of all time and pulls the titles. Just for testing
+# purposes, the user is asked to choose one of the first
+# 26 movies to search for.
+#
+# This is only for the purpose of developing the google
+# search functionality, the real app will be based on user
+# imputs for titles.
+
 link = "https://www.imdb.com/chart/top/"
 
 response = requests.get(link)
@@ -20,47 +30,30 @@ val = input("Which Movie (0 - 25)? ")
 
 title = titles[int(val)]
 txt = title.get_text()
-# print(txt)
 x = txt.split(".")
 txt = x[1]
 txt = txt.strip()
 index = txt.index("\n")
 yearIndex = txt.index("(")
 year = txt[yearIndex+1:-1]
-# print(year)
 txt = txt[0:index].lower()
 words = txt.split(" ")
 words.append(year)
 
-# search_url = ""
-# # search_url = "https://www.reelgood.com/movie/"
-# for i in range(len(words)):
-#     search_url = search_url + words[i]
-#     if i != len(words)-1:
-#         search_url = search_url+"+"
+#######################################################
 
-# print(search_url)
+"""
+Primary Function
+----------------
+Takes in words (movie title) and builds a search
+query to send to google. Then uses a headless (UI-less)
+browser through selenium webDriver to perform the search.
 
-# response = requests.get(search_url)
+Returns a list of objects with the name and links to
+streaming services where the movie is available.
 
-# time.sleep(2)
+"""
 
-# html_doc = response.text
-
-# f = open("thatswhatsup.txt", "w")
-# f.write(html_doc)
-# f.close()
-
-# print(html_doc)
-
-# soup = BeautifulSoup(html_doc,"lxml")
-# officials = soup.findAll("span", class_="css-3g9tm3 e1udhou113")
-# print(len(officials))
-
-# for entry in officials:
-#     print(entry.text)
-
-############################################################
 search_url = "https://www.google.com/search?q="
 for i in range(len(words)):
     search_url = search_url + words[i]
@@ -74,10 +67,6 @@ options.add_argument('headless')
 
 driver = webdriver.Chrome(".\\chromedriver\\chromedriver.exe", options=options)
 
-
-
-# driver = webdriver.Chrome()
-# driver.maximize_window()
 driver.get(search_url)
 
 time.sleep(1)
@@ -88,27 +77,14 @@ platforms = soup.findAll("div", {"class":"ellip bclEt"})
 linkDiv = soup.findAll('div', jsname='gI9xcc')
 links = linkDiv[0].findAll('a')
 
-# x = testststs.find('<a')
-# firstA = testststs[x:]
-# hrefS = firstA.find('href')
-# hrefE = firstA[hrefS:].find('data')
-# print(firstA[hrefS:hrefE])
-
-# print(len(testststs))
-# print(len(links))
 ls = []
-for i in range(len(platforms)):
+
+indeces = len(platforms)
+if len(platforms) > len(links):
+    indeces = len(links)
+
+for i in range(indeces):
     ls.append({"name": platforms[i].text, "link": links[i].get('href')})
 
 print(ls)
 
-# for entry in platforms:
-#     print(entry.text)
-# for link in links:
-#     print(link.get('href'))
-    # print(entry.href)
-
-# # for r in titles:
-#     trades_string = r.get_text()
-#     print(trades_string)
-###################################################################
