df7fce29afd5345d8e5c664035e913010f12b6d2
Jordan <jordanogrinc@gmail.com>
2021-02-16 20:01:17 -0500

    error page hehe

---
+++ public/error/index.php
@@ -0,0 +1,26 @@
+<html>
+  <head></head>
+
+  <body>
+    <nav class= "header navbar navbar-expand-lg sticky-top navbar-dark"></nav>
+		<!-- BODY -->
+
+    <div class="container">
+      <div class="row pt-4 bg-light pb-4 mt-4 rounded text-center">
+        <div class="col-xl-12 col-md-4 text-center">
+            <h1>ERROR 404: PAGE NOT FOUND</h1>
+            <p class="lead">The page you are trying to reach does not exist in this dimension</p>
+            <img src="/src/img/sad_chris.png" class="rounded title" alt="...">
+            <p></p>
+            <p class="lead">While Thor rallies the Avengers to go search the universe for what you were looking for, please try something else...</p>
+        </div>
+      </div>
+    </div>
+
+		<!-- END BODY -->
+		<div class = "footer mt-4 pt-4"></div>
+		<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+		<script src = "/src/js/script.js"></script>
+	</body>
+</html>
