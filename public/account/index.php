895f60aa7276554d3c4828f530cc182ff4a79734
Molly Williams <mollywilliams@Mollys-MacBook-Pro-3.local>
2021-02-22 21:35:41 -0500

    user info and edit

---
+++ public/account/index.php
@@ -0,0 +1,33 @@
+//display user information like services, name, email, ability to change all of them
+
+<html>
+  <head></head>
+
+  <body>
+    <nav class= "header navbar navbar-expand-lg sticky-top navbar-dark"></nav>
+		<!-- BODY -->
+
+    <div class="container col-md-5 mx-auto">
+      <div class="row pt-4 bg-light pb-4 mt-4 rounded">
+        <div class="logo col-md-12 text-center">
+          <h1>Your info</h1>
+          <hr class="hr-or">
+        </div>
+        <div class="col-xs-12 col-md-8 text">
+          <p class="lead"><span class = "font-weight-bold">Name: Bryce</span> </p>
+          <p class="lead"><span class = "font-weight-bold">Email: bryceisgoodatphp@gmail.com</span> </p>
+          <p class="lead"><span class = "font-weight-bold">Current Subscriptions:</span> </p>
+        </div>
+        <div class="col-md-12 text-center">
+          <button type="submit" class="btn btn-block loginbtn btn-primary">Edit Profile</button>
+        </div>
+      </div>
+    </div>
+
+		<!-- END BODY -->
+		<div class = "footer mt-4 pt-4"></div>
+		<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+		<script src = "/src/js/script.js"></script>
+	</body>
+</html>
---
+++ public/editAccount/index.php
@@ -0,0 +1,87 @@
+<html>
+  	<head></head>
+	<body>
+		<nav class= "header navbar navbar-expand-lg sticky-top navbar-dark"></nav>
+
+		<!-- BODY -->
+    	<div class="container mt-5">
+        	<div class="row">
+				<div class="col-md-5 mx-auto">
+					<div class="loginform form">
+						<div class="logo col-md-12 text-center">
+							<h1>Edit Profile</h1>
+						</div>
+						<form action="register.php" method="post" name="register">
+              <input type="hidden" name="register" value=""/>
+              <div class="form-group">
+								<label>Change Name</label>
+								<input type="name" name="email" id="email" class="form-control validate" placeholder="Enter new Name">
+							</div>
+							<div class="form-group">
+								<label>Change Email address</label>
+								<input type="email" name="email" id="email" class="form-control validate" placeholder="Enter Email">
+							</div>
+              <div class="form-group">
+								<label>Edit Subscriptions</label>
+                <div class="row">
+                                    <div class="col-md-4 mb-2">
+                                        <div class="custom-control custom-checkbox image-checkbox">
+                                            <input type="checkbox" class="custom-control-input" name = "netflix" id="ck1a">
+                                            <label class="custom-control-label" for="ck1a">
+                                                <img src="/src/img/netflix.jpg" alt="Netflix" class="img-fluid">
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="custom-control custom-checkbox image-checkbox">
+                                            <input type="checkbox" class="custom-control-input" name = "hulu" id="ck1b">
+                                            <label class="custom-control-label" for="ck1b">
+                                                <img src="/src/img/hulu.png" alt="Hulu" class="img-fluid">
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="custom-control custom-checkbox image-checkbox">
+                                            <input type="checkbox" class="custom-control-input" name = "disney+" id="ck1c">
+                                            <label class="custom-control-label" for="ck1c">
+                                                <img src="/src/img/disneyplus.jpg" alt="Disney+" class="img-fluid">
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="custom-control custom-checkbox image-checkbox">
+                                            <input type="checkbox" class="custom-control-input" name = "prime" id="ck1d">
+                                            <label class="custom-control-label" for="ck1d">
+                                                <img src="/src/img/prime.jpg" alt="Amazon Prime Video" class="img-fluid">
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="custom-control custom-checkbox image-checkbox">
+                                            <input type="checkbox" class="custom-control-input" name = "hbo" id="ck1e">
+                                            <label class="custom-control-label" for="ck1e">
+                                                <img src="/src/img/hbo.png" alt="HBO Max" class="img-fluid">
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+							</div>
+
+							<div class="col-md-12 text-center">
+								<button type="submit" class="btn btn-block loginbtn btn-primary">Save Changes</button>
+							</div>
+
+						</form>
+					</div>
+				</div>
+			</div>
+      	</div>
+
+		<!-- END BODY -->
+		<div class = "footer mt-4 pt-4"></div>
+		<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+		<script src = "/src/js/script.js"></script>
+	</body>
+</html>
