22144bc16bc72256add8d8fa6fa459ff4215ebc9
Samantha Daugherty <samdaugherty26@gmail.com>
2021-02-25 01:28:43 -0500

    added view account details screen

--- public/editAccount/index.php
+++ public/editAccount/index.php
@@ -6,77 +6,133 @@
 		<!-- BODY -->
     	<div class="container mt-5">
         	<div class="row">
-				<div class="col-md-5 mx-auto">
-					<div class="loginform form">
-						<div class="logo col-md-12 text-center">
-							<h1>Edit Profile</h1>
-						</div>
-						<form action="register.php" method="post" name="register">
-              <input type="hidden" name="register" value=""/>
-              <div class="form-group">
-								<label>Change Name</label>
-								<input type="name" name="email" id="email" class="form-control validate" placeholder="Enter new Name">
-							</div>
-							<div class="form-group">
-								<label>Change Email address</label>
-								<input type="email" name="email" id="email" class="form-control validate" placeholder="Enter Email">
-							</div>
-              <div class="form-group">
-								<label>Edit Subscriptions</label>
-                <div class="row">
-                                    <div class="col-md-4 mb-2">
-                                        <div class="custom-control custom-checkbox image-checkbox">
-                                            <input type="checkbox" class="custom-control-input" name = "netflix" id="ck1a">
-                                            <label class="custom-control-label" for="ck1a">
+				<div class="col-md-8 mx-auto">
+                    <div class="loginform form">
+                        <div class="logo col-md-12 text-center">
+                            <h1>Profile</h1>
+                        </div>
+                        <hr />
+                        <div id="view">
+                            <div class="column">
+                                <div class="row">
+                                    <div class="col-sm-5 col-md-4 col-5">
+                                        <label style="font-weight:bold;">Name</label>
+                                    </div>
+                                    <div class="col-md-8 col-6">
+                                        Jeremiah Wasa Bullfrog
+                                    </div>
+                                </div>
+                                <hr />
+                                <div class="row">
+                                    <div class="col-sm-5 col-md-4 col-5">
+                                        <label style="font-weight:bold;">Email</label>
+                                    </div>
+                                    <div class="col-md-8 col-6">
+                                        hewas@goodfriendof.mine
+                                    </div>
+                                </div>
+                                <hr />
+                                <div class="row">
+                                    <div class="col-sm-5 col-md-4 col-5">
+                                        <label style="font-weight:bold;">Subscriptions</label>
+                                    </div>
+                                    <div class="col-md-8 col-6">
+                                        <div class = 'row platforms mb-3'>
+                                            <div class="col-md-3 col-sm-4 mb-2 platformsno">
                                                 <img src="/src/img/netflix.jpg" alt="Netflix" class="img-fluid">
-                                            </label>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <div class="custom-control custom-checkbox image-checkbox">
-                                            <input type="checkbox" class="custom-control-input" name = "hulu" id="ck1b">
-                                            <label class="custom-control-label" for="ck1b">
+                                            </div>
+                                            <div class="col-md-3 col-sm-4 mb-2">
                                                 <img src="/src/img/hulu.png" alt="Hulu" class="img-fluid">
-                                            </label>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <div class="custom-control custom-checkbox image-checkbox">
-                                            <input type="checkbox" class="custom-control-input" name = "disney+" id="ck1c">
-                                            <label class="custom-control-label" for="ck1c">
-                                                <img src="/src/img/disneyplus.jpg" alt="Disney+" class="img-fluid">
-                                            </label>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <div class="custom-control custom-checkbox image-checkbox">
-                                            <input type="checkbox" class="custom-control-input" name = "prime" id="ck1d">
-                                            <label class="custom-control-label" for="ck1d">
-                                                <img src="/src/img/prime.jpg" alt="Amazon Prime Video" class="img-fluid">
-                                            </label>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <div class="custom-control custom-checkbox image-checkbox">
-                                            <input type="checkbox" class="custom-control-input" name = "hbo" id="ck1e">
-                                            <label class="custom-control-label" for="ck1e">
-                                                <img src="/src/img/hbo.png" alt="HBO Max" class="img-fluid">
-                                            </label>
+                                            </div>
+                                            <div class="col-md-3 col-sm-4 mb-2">
+                                                <img src="/src/img/disneyplus.jpg" alt="Hulu" class="img-fluid">
+                                            </div>
+                                            <div class="col-md-3 col-sm-4 mb-2">
+                                                <img src="/src/img/prime.jpg" alt="Hulu" class="img-fluid">
+                                            </div>
+                                            <div class="col-md-3 col-sm-4 mb-2">
+                                                <img src="/src/img/hbo.png" alt="Hulu" class="img-fluid">
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
+                                <div class="col-md-12 text-center">
+                                    <button type="submit" class="btn loginbtn btn-primary" onclick="editDetails()">Edit</button>
+                                </div>
                             </div>
-							</div>
-
-							<div class="col-md-12 text-center">
-								<button type="submit" class="btn btn-block loginbtn btn-primary">Save Changes</button>
-							</div>
-
-						</form>
+                        </div>
+                    
+                        <div id="edit" style="display:none">
+                            <form>
+                                <div class="form-group">
+                                    <label>Change Name</label>
+                                    <input type="name" name="name" id="name" class="form-control validate" value="Jeremiah Wasa Bullfrog">
+                                </div>
+                                <div class="form-group">
+                                    <label>Change Email address</label>
+                                    <input type="email" name="email" id="email" class="form-control validate" value="hewas@goodfriendof.mine">
+                                </div>
+                                <div class="form-group">
+                                    <label>Edit Subscriptions</label>
+                                    <div class="row">
+                                        <div class="col-md-3 col-sm-4 mb-2">
+                                            <div class="custom-control custom-checkbox image-checkbox">
+                                                <input type="checkbox" class="custom-control-input" name = "netflix" id="ck1a">
+                                                <label class="custom-control-label" for="ck1a">
+                                                    <img src="/src/img/netflix.jpg" alt="Netflix" class="img-fluid">
+                                                </label>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-3 col-sm-4 mb-2">
+                                            <div class="custom-control custom-checkbox image-checkbox">
+                                                <input type="checkbox" class="custom-control-input" name = "hulu" id="ck1b">
+                                                <label class="custom-control-label" for="ck1b">
+                                                    <img src="/src/img/hulu.png" alt="Hulu" class="img-fluid">
+                                                </label>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-3 col-sm-4 mb-2">
+                                            <div class="custom-control custom-checkbox image-checkbox">
+                                                <input type="checkbox" class="custom-control-input" name = "disney+" id="ck1c">
+                                                <label class="custom-control-label" for="ck1c">
+                                                    <img src="/src/img/disneyplus.jpg" alt="Disney+" class="img-fluid">
+                                                </label>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-3 col-sm-4 mb-2">
+                                            <div class="custom-control custom-checkbox image-checkbox">
+                                                <input type="checkbox" class="custom-control-input" name = "prime" id="ck1d">
+                                                <label class="custom-control-label" for="ck1d">
+                                                    <img src="/src/img/prime.jpg" alt="Amazon Prime Video" class="img-fluid">
+                                                </label>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-3 col-sm-4 mb-2">
+                                            <div class="custom-control custom-checkbox image-checkbox">
+                                                <input type="checkbox" class="custom-control-input" name = "hbo" id="ck1e">
+                                                <label class="custom-control-label" for="ck1e">
+                                                    <img src="/src/img/hbo.png" alt="HBO Max" class="img-fluid">
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-12 text-center">
+                                    <button type="submit" class="btn btn-block loginbtn btn-primary">Save Changes</button>
+                                </div>
+						    </form>
+                        </div>
 					</div>
 				</div>
 			</div>
       	</div>
+        
+        <script>
+            function editDetails() {
+                document.getElementById("view").style.display = "none";
+                document.getElementById("edit").style.display = "inline";
+            }
+        </script>
 
 		<!-- END BODY -->
 		<div class = "footer mt-4 pt-4"></div>
