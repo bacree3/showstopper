cd7a3b44ebe0eda259f73532db2c1e70a5ef7497
bryceacree <bryce@acreetek.com>
2021-03-05 10:28:05 -0500

    adding example post request in test

--- public/test.php
+++ public/test.php
@@ -3,21 +3,34 @@
 include $_SERVER['DOCUMENT_ROOT'] . '/php/auth.php';
 
 $url = 'https://zveblvb4u3.execute-api.us-east-1.amazonaws.com/getActors';
-//$data = array('key1' => 'value1', 'key2' => 'value2');
-
-// use key 'http' even if you send the request to https://...
-/*$options = array(
-    'http' => array(
-        //'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-        'method'  => 'POST',
-        //'content' => http_build_query($data)
-    )
+// The data to send to the API
+$postData = array(
+    'id' => 'tt0167404',
 );
-$context  = stream_context_create($options);
-$result = file_get_contents($url, false, $context);
-if ($result === FALSE)
 
-var_dump($result); */
+// Create the context for the request
+$context = stream_context_create(array(
+    'http' => array(
+        // http://www.php.net/manual/en/context.http.php
+        'method' => 'POST',
+        'header' => "Content-Type: application/json\r\n",
+        'content' => json_encode($postData)
+    )
+));
+
+// Send the request
+$response = file_get_contents($url, FALSE, $context);
+
+// Check for errors
+if($response === FALSE){
+    die('Error');
+}
+
+// Decode the response
+$responseData = json_decode($response, TRUE);
+
+// Print the date from the response
+print_r($responseData);
 
 //echo passwordResetAllowed('6033f04273227');
 
@@ -130,11 +143,11 @@ echo $result;*/
 </html>
 
 <script>
-function test() {
+/*function test() {
 	console.log("function started");
 	var returnObject = {};
 	var dataObject = {
-		id: "the avengers"
+		id: "tt0167404"
 	};
 	var apiURL = 'https://zveblvb4u3.execute-api.us-east-1.amazonaws.com/getActors';
 	$.ajax({
@@ -157,5 +170,5 @@ function test() {
 	console.log("function ended.");
 	return returnObject;
 }
-console.log(test());
+console.log(test()); */
 </script>
