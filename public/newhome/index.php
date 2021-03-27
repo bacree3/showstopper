014d5acbd5e06dde02bc653326abe01eee49d0ce
Jordan <jordanogrinc@gmail.com>
2021-03-27 14:03:28 -0400

    start of home redesign

---
+++ public/newhome/index.php
@@ -0,0 +1,24 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT'] . '/php/auth.php';
+
+?>
+<html>
+  <head></head>
+  <link rel="stylesheet" href="css/style0.css">
+  <body id = "home">
+    <nav class="header navbar navbar-expand-lg navbar-dark"></nav>
+
+    <!-- BODY -->
+    <div class = "container align-items-center">
+      <h1 class="display-4 text-white align-center">ShowStopper</h1>
+    </div>
+
+
+
+    <!-- END BODY -->
+    <div class = "footer mt-4 pt-4"></div>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+		<script src = "/src/js/script.js"></script>
+	</body>
+</html>
