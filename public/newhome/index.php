5a62a14e54e88e6338a277449da061618a35a165
Jordan <jordanogrinc@gmail.com>
2021-03-27 17:20:20 -0400

    home redesign feeling meh

--- public/newhome/index.php
+++ public/newhome/index.php
@@ -9,14 +9,42 @@ include $_SERVER['DOCUMENT_ROOT'] . '/php/auth.php';
     <nav class="header navbar navbar-expand-lg navbar-dark"></nav>
 
     <!-- BODY -->
-    <div class = "container align-items-center">
-      <h1 class="display-4 text-white align-center">ShowStopper</h1>
+    <div class="row">
+      <div class="col-sm justify-content-center mt-4">
+        <h1 class="display-2 text-white center">ShowStopper</h1>
+        <h3 class="lead center text-white">Get the most out of your streaming platforms with JIA 0306 </h1>
+      </div>
+      <div class="col-sm justify-content-center">
+        <img src="/src/img/homecomp.png" class = "homecomp">
+      </div>
+    </div>
+    <div class="row text-center ml-4 mr-4">
+      <div class="col-xs-6 col-md-3">
+        <div class="">
+          <img src="/src/img/avengers.jpg" class="rounded title" alt="...">
+        </div>
+      </div>
+      <div class="col-xs-6 col-md-3">
+        <div>
+          <img src="/src/img/star-wars.jpg" class="rounded title" alt="...">
+        </div>
+      </div>
+      <div class="col-xs-6 col-md-3">
+        <div>
+          <img src="/src/img/harry-potter.jpg" class="rounded title" alt="...">
+        </div>
+      </div>
+      <div class="col-xs-6 col-md-3">
+        <div>
+          <img src="/src/img/extraction.jpg" class="rounded title" alt="...">
+        </div>
+      </div>
     </div>
 
 
 
     <!-- END BODY -->
-    <div class = "footer mt-4 pt-4"></div>
+    <div class = "footer mt<img src="/src/img/avengers.jpg" class="rounded title" alt="...">-4 pt-4"></div>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
 		<script src = "/src/js/script.js"></script>
