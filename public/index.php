cd04d559c563a9f8520f5e83a022a97c6390dc80
Jordan <jordanogrinc@gmail.com>
2021-03-28 20:07:39 -0400

    added reccomendation list to homepage

--- public/index.php
+++ public/index.php
@@ -20,32 +20,74 @@ include $_SERVER['DOCUMENT_ROOT'] . '/php/auth.php';
     </div>
 
     <div class="container">
-      <div class="row text-center">
-        <div class="col-xs-6 col-md-3">
+      <div class="row pb-4">
+        <div class="col-2 pt-4 text-center">
           <div class="">
-            <img src="/src/img/avengers.jpg" class="rounded title" alt="...">
+            <h3 class="text-white">Trending</p>
           </div>
         </div>
-        <div class="col-xs-6 col-md-3">
-          <div>
-            <img src="/src/img/star-wars.jpg" class="rounded title" alt="...">
+        <div class="col-2 ">
+          <div class="">
+            <img src="/src/img/avengers.jpg" class="rounded title homemoviepic" alt="...">
           </div>
         </div>
-        <div class="col-xs-6 col-md-3">
-          <div>
-            <img src="/src/img/harry-potter.jpg" class="rounded title" alt="...">
+        <div class="col-2">
+          <div class="">
+            <img src="/src/img/avengers.jpg" class="rounded title homemoviepic" alt="...">
           </div>
         </div>
-        <div class="col-xs-6 col-md-3">
+        <div class="col-2">
           <div>
-            <img src="/src/img/extraction.jpg" class="rounded title" alt="...">
+            <img src="/src/img/star-wars.jpg" class="rounded title homemoviepic" alt="...">
+          </div>
+        </div>
+        <div class="col-2">
+          <div>
+            <img src="/src/img/harry-potter.jpg" class="rounded title homemoviepic" alt="...">
+          </div>
+        </div>
+        <div class="col-2">
+          <div>
+            <img src="/src/img/extraction.jpg" class="rounded title homemoviepic" alt="...">
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-2 pt-4 text-center">
+          <div class="">
+            <h3 class="text-white">Reccomended For You</p>
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="">
+            <img src="/src/img/avengers.jpg" class="rounded title homemoviepic" alt="...">
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="">
+            <img src="/src/img/avengers.jpg" class="rounded title homemoviepic" alt="...">
+          </div>
+        </div>
+        <div class="col-2">
+          <div>
+            <img src="/src/img/star-wars.jpg" class="rounded title homemoviepic" alt="...">
+          </div>
+        </div>
+        <div class="col-2">
+          <div>
+            <img src="/src/img/harry-potter.jpg" class="rounded title homemoviepic" alt="...">
+          </div>
+        </div>
+        <div class="col-2">
+          <div>
+            <img src="/src/img/extraction.jpg" class="rounded title homemoviepic" alt="...">
           </div>
         </div>
       </div>
     </div>
 
     <!-- END BODY -->
-    <div class = "footer mt-4 pt-4"></div>
+    <!--<div class = "footer mt-4 pt-4"></div> -->
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
 		<script src = "/src/js/script.js"></script>
