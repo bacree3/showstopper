744a5504ddac5189fef51e9e3ddd7e0f4eb555b2
bryceacree <bryce@acreetek.com>
2021-04-23 11:49:38 -0400

    added example parameters file

---
+++ php/parameters-example.php
@@ -0,0 +1,13 @@
+<?php
+
+$ip = 'YOUR_SQL_URL_ADDRESS';
+$user = 'YOUR_SQL_USERNAME';
+$password = 'YOUR_SQL_USERS_PASSWORD';
+$schema = "dev";
+$omdbKey = 'YOUR_OMBD_KEY';
+$omdbURL = "http://www.omdbapi.com/?apikey=" . $omdbKey . "&";
+$SMTPuser = 'YOUR_SES_EMAIL_USER';
+$SMTPsecret = 'YOUR_SES_EMAIL_PASSWORD';
+$apiURL = 'CUSTOM_API_URL';
+
+?>
