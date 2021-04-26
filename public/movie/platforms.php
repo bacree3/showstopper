aeeadc8a21ff2606dc881ee3fd43e64cfc5092f3
bryceacree <bryce@acreetek.com>
2021-04-25 22:05:49 -0400

    removed old test comments

--- public/movie/platforms.php
+++ public/movie/platforms.php
@@ -5,9 +5,6 @@ include $_SERVER['DOCUMENT_ROOT'] . '/php/functions.php';
 if (isset($_GET['id']) && isset($_GET['name'])) {
 	$name = $_GET['name'];
 	$id = $_GET['id'];
-	//echo $name;
-	//$title = 'Avengers: Age of Ultron';
-	//$id = 'tt2395427';
 	$platforms = scrapePlatforms($name);
 	insertPlatforms($id, $platforms);
 	//if ($_GET['update']) {
